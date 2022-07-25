9696227ca204f23d34d14ec565e155298082c63b
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-25 17:28:19 +0900

    fin profile pic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { Toc } from '@mui/icons-material';
 import './App.css';
+import { Toc } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar_container">
         <div className="sidebar">
-          <Toc />
+          <div className="lines_icon">
+            <Toc />
+          </div>
+          <div className="profile">
+            <img
+              src="https://media.bunjang.co.kr/product/177600551_1_1643548856_w360.jpg"
+              alt="profile_pic"
+            />
+          </div>
         </div>
       </div>
       <div className="body_container"></div>
